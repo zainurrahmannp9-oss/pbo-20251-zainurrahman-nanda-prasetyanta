8f2ad5d24a828dc7f9aea989dc461fbebac66593
zainurrahmannp9-oss <zainurrahmannp9@gmail.com>
2025-11-13 08:10:52 +0700

    feat: namespace

--- property_constant/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('./LuasLingkaran.php');
-
-use App\Math\LuasLingkaran;
-
-$lingkaran = new LuasLingkaran(10);
-// $lingkaran->jari = 10; //input nilai jari-jari
-$lingkaran->tampil("roda"); //tampilkan luas lingkaran
